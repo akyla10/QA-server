83e23480bb73c069adc7d4000b569f3e98d7323f
Anton Kuleshov <kuleshov@my.com>
2014-03-26 19:59:30 +0400

    никчемный коммит

---
+++ src/test/Resource/RatingTest.java
@@ -0,0 +1,17 @@
+package Resource;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import resource.Rating;
+
+/**
+ * Created by anton on 3/26/14.
+ */
+public class RatingTest {
+
+    @Test
+    public void simpleTest() {
+        Rating r = new Rating();
+        Assert.assertTrue(true);
+    }
+}
