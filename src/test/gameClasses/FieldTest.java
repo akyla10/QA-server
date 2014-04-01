135ab089f8a85e0d55176782ccdd906e1c746c51
Anton Kuleshov <kuleshov@my.com>
2014-04-02 00:43:43 +0400

    Field

--- src/test/gameClasses/FieldTest.java
+++ src/test/gameClasses/FieldTest.java
@@ -10,31 +10,15 @@ import org.junit.Test;
 public class FieldTest {
     Field field;
 
-    @Before
-    public void init() {
-        field = new Field();
-    }
-
     @Test
-    public void testNothing() {
-        field.setType(Field.checker.nothing);
-        Assert.assertTrue(field.getType().equals(Field.checker.nothing));
-    }
-
-    @Test
-    public void testWhite() {
-        field.setType(Field.checker.white);
-        Assert.assertTrue(field.getType().equals(Field.checker.white));
-    }
-
-    @Test
-    public void testBlack() {
-        field.setType(Field.checker.black);
-        Assert.assertTrue(field.getType().equals(Field.checker.black));
-    }
-
-    public void testNewFieldIsEmpty() {
+    public void testNewFieldIsNotEmpty() {
         Field f = new Field(Field.checker.white);
+        Assert.assertFalse(f.isEmpty());
+    }
+
+    @Test
+    public void testNewFieldIsEmpty() {
+        Field f = new Field(Field.checker.nothing);
         Assert.assertTrue(f.isEmpty());
     }
 
