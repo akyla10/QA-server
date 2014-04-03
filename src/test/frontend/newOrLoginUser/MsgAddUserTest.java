59350b0c20b7830c796b27c459b61a7c4cd080d6
Anton Kuleshov <akyla10@yandex.ru>
2014-04-04 00:17:17 +0400

    Заменил свои имплементации с барышнями и преферансом на моки.

--- src/test/frontend/newOrLoginUser/MsgAddUserTest.java
+++ src/test/frontend/newOrLoginUser/MsgAddUserTest.java
@@ -4,20 +4,37 @@ import base.*;
 import dbService.UserDataSet;
 import junit.framework.Assert;
 import junit.framework.TestCase;
+import messageSystem.MessageSystemImpl;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * Created by anton on 4/1/14.
  */
 public class MsgAddUserTest {
 
+    private DataAccessObject dataAccessObject;
+    private MessageSystem messageSystem;
+
+    @Before
+    public void init() {
+        dataAccessObject = mock(DataAccessObject.class);
+        when(dataAccessObject.addUDS("true", "")).thenReturn(true);
+        when(dataAccessObject.addUDS("false", "")).thenReturn(false);
+        messageSystem = mock(MessageSystem.class);
+        when(dataAccessObject.getMessageSystem()).thenReturn(messageSystem);
+        when(dataAccessObject.getUDS("true","")).thenReturn(new UserDataSet());
+    }
+
     @Test
     public void execTestExist() {
-        DataAccessObjectImpl dataAccessObject = new DataAccessObjectImpl();
         MsgAddUser msgAddUser = new MsgAddUser(null, null, "", "true", "" );
         UserDataSet userDataSet =  msgAddUser.exec(dataAccessObject);
         Assert.assertNotNull(userDataSet);
@@ -25,80 +42,8 @@ public class MsgAddUserTest {
 
     @Test
     public void execTestNotExist() {
-        DataAccessObjectImpl dataAccessObject = new DataAccessObjectImpl();
-        MsgAddUser msgAddUser = new MsgAddUser(null, null, "", "true1", "" );
+        MsgAddUser msgAddUser = new MsgAddUser(null, null, "", "false", "" );
         UserDataSet userDataSet =  msgAddUser.exec(dataAccessObject);
         Assert.assertNull(userDataSet);
-    }
-
-
-
-    private class DataAccessObjectImpl implements DataAccessObject {
-
-        @Override
-        public MessageSystem getMessageSystem() {
-            MSImpl ms = new MSImpl();
-            return ms;
-        }
-
-        @Override
-        public UserDataSet getUDS(String login, String password) {
-            UserDataSet userDataSet = new UserDataSet();
-            return userDataSet;
-        }
-
-        @Override
-        public boolean addUDS(String login, String password) {
-            if (login.equals("true")) {
-                return true;
-            }
-            return false;
-        }
-
-        @Override
-        public void updateUsers(List<UserDataSet> users) {
-
-        }
-
-        @Override
-        public Address getAddress() {
-            Address address = new Address();
-            return null;
-        }
-
-        @Override
-        public void run() {
-
-        }
-
-        public class MSImpl implements MessageSystem {
-            @Override
-            public void addService(Abonent abonent, String name) {
-
-            }
-
-            @Override
-            public Address getAddressByName(String name) {
-                return null;
-            }
-
-            @Override
-            public Map<Address, ConcurrentLinkedQueue<Msg>> getMessages() {
-                return null;
-            }
-
-            @Override
-            public void putMsg(Address to, Msg msg) {
-
-            }
-
-            @Override
-            public void execForAbonent(Abonent abonent) {
-
-            }
-
-            ;
-        }
-
     }
 }
