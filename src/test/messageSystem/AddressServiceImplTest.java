76a2f1f5bad4957e0953b37aad198e211086f615
Anton Kuleshov <akyla10@yandex.ru>
2014-04-09 14:43:23 +0400

    some tests in adressserviceimpl

--- src/test/messageSystem/AddressServiceImplTest.java
+++ src/test/messageSystem/AddressServiceImplTest.java
@@ -2,14 +2,20 @@ package messageSystem;
 
 import base.Abonent;
 import base.Address;
+import base.MessageSystem;
 import junit.framework.Assert;
 import messageSystem.AddressServiceImpl;
 import messageSystem.MessageSystemImpl;
 import org.junit.Before;
 import org.junit.Test;
+import sun.management.resources.agent_fr;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -41,4 +47,42 @@ public class AddressServiceImplTest {
 
         Assert.assertEquals(null, addressService.getAddressByName("nonExistingName"));
     }
+
+    @Test
+    public void test() throws NoSuchFieldException, IllegalAccessException {
+        MessageSystem messageSystem = mock(MessageSystem.class);
+        AddressServiceImpl addressService1 = new AddressServiceImpl();
+        Field field = addressService1.getClass().getDeclaredField("nameToQuantity");
+        field.setAccessible(true);
+        Map<String,Integer> nameToQuantity = mock( HashMap.class);
+        when(nameToQuantity.containsKey(anyString())).thenReturn(true);
+        when(nameToQuantity.get(anyString())).thenReturn(1);
+//        nameToQuantity.put("opop", 12);
+        field.set(addressService1, nameToQuantity);
+        Abonent abonent1 = mock(Abonent.class);
+        when(abonent1.getAddress()).thenReturn(new Address());
+        addressService1.addService(abonent1, "1");
+        verify(nameToQuantity, never()).put(anyString(), eq(1));
+    }
+
+    @Test
+    public void test1() throws NoSuchFieldException, IllegalAccessException {
+        MessageSystem messageSystem = mock(MessageSystem.class);
+        AddressServiceImpl addressService1 = new AddressServiceImpl();
+        Field field = addressService1.getClass().getDeclaredField("nameToQuantity");
+        Map<String,Integer> nameToQuantity = mock( HashMap.class);
+        field.setAccessible(true);
+        when(nameToQuantity.containsKey(anyString())).thenReturn(true);
+        when(nameToQuantity.get(anyString())).thenReturn(2);
+        field.set(addressService1, nameToQuantity);
+        Map<String,Integer> nameToLast = mock( HashMap.class);
+        field = addressService1.getClass().getDeclaredField("nameToLast");
+        field.setAccessible(true);
+        field.set(addressService1, nameToLast);
+        Abonent abonent1 = mock(Abonent.class);
+        when(abonent1.getAddress()).thenReturn(new Address());
+        addressService1.addService(abonent1, "1");
+        verify(nameToLast,never()).put(anyString(),eq(1));
+    }
+
 }
